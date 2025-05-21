a280baef3f3d366be8c9b3e0275ab4c10ac03831
Prakash Jat <prakashjatt966@gmail.com>
2025-05-21 19:30:48 +0530

    Integrate the No Dues Rejection Reason

--- src/components/AuthorityDashBoardTables/ApprovedStudentsTable.jsx
+++ src/components/AuthorityDashBoardTables/ApprovedStudentsTable.jsx
@@ -227,7 +227,7 @@ const ApprovedStudentsTable = ({ authorityFromParent }) => {
               required
               style={{ background: "#f8f9fa" }}
             >
-              <option value="" disabled selected>
+              <option value="" disabled >
                 -- Select a reason --
               </option>
               <option>Fee receipt required</option>
